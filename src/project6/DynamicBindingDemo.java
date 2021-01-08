c715bf7944ac4f2e216d9625e62cdc936d6ba8ee
FPhoenixtree <309890974@qq.com>
2021-01-08 11:54:35 +0800

    提交

---
+++ src/project6/DynamicBindingDemo.java
@@ -0,0 +1,10 @@
+package project6;
+
+public class DynamicBindingDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/project6/RectangleFromSimpleGeometricObject.java
@@ -0,0 +1,10 @@
+package project6;
+
+public class RectangleFromSimpleGeometricObject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
